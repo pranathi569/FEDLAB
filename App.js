8d34922f375b75afd17e1af441102d0cf31ee007
pranathi569 <141762913+pranathi569@users.noreply.github.com>
2023-09-02 11:44:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Counter from './Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
