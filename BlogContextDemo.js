fec8e383fdf67aa95e21b79fae4156a227f391ee
pranathi569 <141762913+pranathi569@users.noreply.github.com>
2023-10-07 10:53:44 +0530

    Add files via upload

---
+++ BlogContextDemo.js
@@ -0,0 +1,14 @@
+import React,{useContext} from 'react'
+import {BlogContext} from './App'
+function BlogContextDemo(){
+    const binfo=useContext(BlogContext);
+    return(
+        <div>
+            <p><h2>Topic: {binfo.React.post}</h2></p>
+            <p><h2>Author: {binfo.React.author}</h2></p>
+            <p><h2>Topic: {binfo.Nodejs.post}</h2></p>
+            <p><h2>Author: {binfo.Nodejs.author}</h2></p>
+        </div>
+    )
+}
+export default BlogContextDemo;
