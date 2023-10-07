2ac4a910dcfb4b5145ad057d224e256732e86d15
pranathi569 <141762913+pranathi569@users.noreply.github.com>
2023-10-07 10:54:34 +0530

    Add files via upload

---
+++ Parent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Child from './Child'
+function Parent(){
+    return(<div>
+        <Child color="red" backColor="green"/>
+    </div>
+    );
+}
+export default Parent;
